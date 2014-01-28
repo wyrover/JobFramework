dcd64dd05b2293fc0810b75db4eaace7bb25330f
guodongdong <dd.guo@foxmail.com>
2014-01-28 10:55:01 +0800

    add clients, modules, extension modules

---
+++ jobframework/extensions/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python2.6
+# encoding: utf-8
+# Author: guodongdong <dd.guo@foxmail.com>
+# Created Time: 2014年01月28日 星期二 10时54分41秒
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
